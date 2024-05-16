bab96a142873ec0a626a52ab5dcd068f225dc554
CHARLES EMANYO <charles.emanyo@saed.dev>
2024-05-16 09:35:50 +0100

    Added Password Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LanguageButton from './components/LanguageButton';
 import './styles/index.css';
+import PasswordCard from './components/PasswordCard';
 
 const App: React.FC = () => {
   return (
     <div className='App'>
       <header className='App-header'>
         <LanguageButton />
+        <PasswordCard />
       </header>
     </div>
   );
