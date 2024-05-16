63d64ef1c4ff920f102a3455e736a1781b078837
CHARLES EMANYO <charles.emanyo@saed.dev>
2024-05-16 09:36:45 +0100

    added new icons along with a special file housing all exports

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,5 @@
 export { default as FranceFlag } from './FranceFlag';
 export { default as USFlag } from './USFlag';
+export { default as Search } from './Search';
+export { default as EyeOpen } from './EyeOpen';
+export { default as EyeClose } from './EyeClose';
