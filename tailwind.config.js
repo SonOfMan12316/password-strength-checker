f78cb657c4384ea68f44623b83cf25931acb847f
CHARLES EMANYO <charles.emanyo@saed.dev>
2024-05-16 09:41:37 +0100

    more dependecies added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'ch-primary': '#83BF4F',
+        'ch-primary': '#1C5691',
         'ch-secondary': '#83BF4F',
         'ch-tertiary': '#1b3c77',
         'ch-danger': '#fe0808',
         'ch-grey': '#f8f8f8',
         'ch-border-grey': '#999999',
-        'ch-trans-secondary': '#6cbd454d',
+        'ch-trans-secondary': '#202124',
         'ch-footer': '#333333',
         'ch-footer-paragraph': '#FFFFE6',
       },
