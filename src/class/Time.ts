58d2322af0e67ef9cb79746eb0b0258c43162ed3
CHARLES EMANYO <charles.emanyo@saed.dev>
2024-05-31 11:13:24 +0100

    fixed change preventing build

--- src/class/Time.ts
+++ src/class/Time.ts
@@ -3,14 +3,14 @@ export default class Time {
     if (seconds <= 60) {
       // 1minute
       return {
-        fr: "Votre mot de passe pourrait être craqué avant même que vous n'ayez le temps de dire 'mancyDev'",
-        en: "Your password could be cracked before you even have seconds to say 'mancyDev'",
+        fr: 'Votre mot de passe pourrait être craqué avant même que vous n\'ayez le temps de dire \'mancyDev\'',
+        en: 'Your password could be cracked before you even have seconds to say \'mancyDev\'',
       };
     } else if (seconds <= 600) {
       // 10minute
       return {
         fr:
-          "Pourquoi le mot de passe a-t-il rougi ? Parce qu'il a entendu qu'il serait craqué en " +
+          'Pourquoi le mot de passe a-t-il rougi ? Parce qu\'il a entendu qu\'il serait craqué en ' +
           Math.floor(seconds / 60) * 25 +
           'minutes!',
         en:
@@ -47,7 +47,7 @@ export default class Time {
       // 24hours
       return {
         fr:
-          "C'est le temps pour télécharger un fichier de " +
+          'C\'est le temps pour télécharger un fichier de ' +
           Math.floor(24 * (seconds / 3600)) +
           'Mo avec un modem 56K',
         en:
@@ -71,7 +71,7 @@ export default class Time {
       // 1week
       return {
         fr:
-          "C'est le temps pour télécharger " +
+          'C\'est le temps pour télécharger ' +
           Math.floor((450 * (seconds / 3600)) / 1000) +
           ' films légalement avec une connexion moyenne',
         en:
@@ -82,18 +82,18 @@ export default class Time {
     } else if (seconds <= 1209600) {
       // 2weeks
       return {
-        fr: "C'est le temps nécessaire pour graver " + Math.floor(seconds / (15 * 60)) + ' DVD',
+        fr: 'C\'est le temps nécessaire pour graver ' + Math.floor(seconds / (15 * 60)) + ' DVD',
         en: 'This is the seconds to burn ' + Math.floor(seconds / (15 * 60)) + ' DVDs',
       };
     } else if (seconds <= 2419200) {
       // 1months
       return {
         fr:
-          "C'est assez pour faire " +
+          'C\'est assez pour faire ' +
           Math.floor((seconds / 30000) * 2) +
           ' vols aller retour Paris - New York',
         en:
-          "It's enough to make " +
+          'It\'s enough to make ' +
           Math.floor((seconds / 30000) * 2) +
           ' return flights Paris - New York',
       };
@@ -101,7 +101,7 @@ export default class Time {
       // 6months
       return {
         fr:
-          "C'est le temps que met un romancier pour écrire " +
+          'C\'est le temps que met un romancier pour écrire ' +
           Math.floor(seconds / 25920) +
           ' pages de son roman',
         en:
@@ -113,7 +113,7 @@ export default class Time {
       // 2years
       return {
         fr:
-          "C'est le temps nécessaire pour construire " +
+          'C\'est le temps nécessaire pour construire ' +
           Math.floor(seconds / 18408600) +
           ' maisons',
         en: 'This is the seconds needed to build ' + Math.floor(seconds / 18408600) + ' houses',
@@ -133,16 +133,16 @@ export default class Time {
     } else if (seconds <= 290304000) {
       // 10years
       return {
-        fr: "C'est le délai de construction de " + Math.abs(seconds / 94608000) + ' RMS Titanic',
+        fr: 'C\'est le délai de construction de ' + Math.abs(seconds / 94608000) + ' RMS Titanic',
         en: 'This is the seconds to build ' + Math.abs(seconds / 94608000) + ' RMS Titanic',
       };
     } else if (seconds <= 11612160000) {
       // 400years
       return {
         fr:
-          "C'est le temps qu'il faut pour réaliser " +
+          'C\'est le temps qu\'il faut pour réaliser ' +
           Math.floor(seconds / 378432000) +
-          " nouvelles fusées (de la fabrication des premiers prototypes jusqu'au premier lancement)",
+          ' nouvelles fusées (de la fabrication des premiers prototypes jusqu\'au premier lancement)',
         en:
           'This is the seconds it takes to make ' +
           Math.floor(seconds / 378432000) +
@@ -152,7 +152,7 @@ export default class Time {
       // 20000years
       return {
         fr:
-          "C'est le temps nécessaire pour envoyer des hommes faire " +
+          'C\'est le temps nécessaire pour envoyer des hommes faire ' +
           Math.floor(seconds / ((78624000 + 31536000) * 2)) +
           ' aller retour Terre - Mars',
         en:
@@ -162,7 +162,7 @@ export default class Time {
       };
     } else {
       return {
-        fr: "Vous pouvez trouver la réponse à la Grande Question sur la vie, l'univers et le reste sans avoir à vous préoccuper pour votre mot de passe.",
+        fr: 'Vous pouvez trouver la réponse à la Grande Question sur la vie, l\'univers et le reste sans avoir à vous préoccuper pour votre mot de passe.',
         en: 'You can find the answer to the Great Question about life, the universe and the rest without having to worry about your password.',
       };
     }
